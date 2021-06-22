4b1c77dfa7c0369aee672aa6adbba040c9c55426
Anushreya Satish <75848533+Anushreya-Satish@users.noreply.github.com>
2021-06-22 11:11:58 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,73 @@
+playSound("assets/The-Miraculous-Rush.mp3", true);
+onEvent("button1", "click", function( ) {
+	setScreen("screen2");
+});
+onEvent("radio_button2", "change", function( ) {
+	setScreen("screen3");
+});
+onEvent("radio_button8", "change", function( ) {
+	setScreen("screen4");
+});
+onEvent("radio_button9", "change", function( ) {
+	setScreen("screen5");
+});
+onEvent("button2", "click", function( ) {
+	setScreen("screen1");
+});
+onEvent("radio_button1", "change", function( ) {
+	setScreen("screen6");
+});
+onEvent("radio_button14", "change", function( ) {
+	setScreen("screen7");
+});
+onEvent("button2", "click", function( ) {
+ setScreen("screen1");
+});
+onEvent("radio_button55", "change", function( ) {
+ setScreen("screen21");
+});
+onEvent("label54", "click", function( ) {
+	setScreen("screen20");
+});
+onEvent("radio_button19", "change", function( ) {
+ setScreen("screen9");
+});
+onEvent("radio_button24", "change", function( ) {
+ setScreen("screen10");
+});
+onEvent("radio_button25", "change", function( ) {
+ setScreen("screen11");
+});
+onEvent("radio_button28", "change", function( ) {
+ setScreen("screen12");
+});
+onEvent("radio_button17", "change", function( ) {
+ setScreen("screen8");
+});
+onEvent("radio_button31", "change", function( ) {
+ setScreen("screen13");
+});
+onEvent("radio_button35", "change", function( ) {
+ setScreen("screen14");
+});
+onEvent("radio_button38", "change", function( ) {
+ setScreen("screen15");
+});
+onEvent("radio_button42", "change", function( ) {
+ setScreen("screen16");
+});
+onEvent("radio_button44", "change", function( ) {
+ setScreen("screen17");
+});
+onEvent("radio_button48", "change", function( ) {
+ setScreen("screen18");
+});
+onEvent("radio_button51", "change", function( ) {
+ setScreen("screen19");
+});
+onEvent("radio_button54", "change", function( ) {
+ setScreen("screen20");
+});
+onEvent("button3", "click", function( ) {
+	open("https://anushreyasatish.whjr.site/");
+});
